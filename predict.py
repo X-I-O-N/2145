126e91f17794ec23544ca1b16dfe50d22985ba50
X-I-O-N <teerth9000@gmail.com>
2017-06-15 01:17:36 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -26,7 +26,91 @@ from sklearn import svm
 import pickle
 
 # <codecell>
+def tied_rank(x):
+    """
+    This function is by Ben Hamner and taken from https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/auc.py
 
+    Computes the tied rank of elements in x.
+
+    This function computes the tied rank of elements in x.
+
+    Parameters
+    ----------
+    x : list of numbers, numpy array
+
+    Returns
+    -------
+    score : list of numbers
+            The tied rank f each element in x
+
+    """
+    sorted_x = sorted(zip(x,range(len(x))))
+    r = [0 for k in x]
+    cur_val = sorted_x[0][0]
+    last_rank = 0
+    for i in range(len(sorted_x)):
+        if cur_val != sorted_x[i][0]:
+            cur_val = sorted_x[i][0]
+            for j in range(last_rank, i): 
+                r[sorted_x[j][1]] = float(last_rank+1+i)/2.0
+            last_rank = i
+        if i==len(sorted_x)-1:
+            for j in range(last_rank, i+1): 
+                r[sorted_x[j][1]] = float(last_rank+i+2)/2.0
+    return r
+
+def auc(actual, posterior):
+    """
+    This function is by Ben Hamner and taken from https://github.com/benhamner/Metrics/blob/master/Python/ml_metrics/auc.py
+    
+    Computes the area under the receiver-operater characteristic (AUC)
+
+    This function computes the AUC error metric for binary classification.
+
+    Parameters
+    ----------
+    actual : list of binary numbers, numpy array
+             The ground truth value
+    posterior : same type as actual
+                Defines a ranking on the binary numbers, from most likely to
+                be positive to least likely to be positive.
+
+    Returns
+    -------
+    score : double
+            The mean squared error between actual and posterior
+
+    """
+    r = tied_rank(posterior)
+    num_positive = len([0 for x in actual if x==1])
+    num_negative = len(actual)-num_positive
+    sum_positive = sum([r[i] for i in range(len(r)) if actual[i]==1])
+    auc = ((sum_positive - num_positive*(num_positive+1)/2.0) /
+           (num_negative*num_positive))
+    sys.stdout.write('.')
+    return auc
+
+def auc_scorer(estimator, X, y):
+    predicted = estimator.predict_proba(X)[:,1]
+    return auc(y, predicted)
+       
+def normalize10day(stocks):
+    def process_column(i):
+        if operator.mod(i, 5) == 1:
+            return stocks[:,i] * 0
+        if operator.mod(i, 5) == 2:
+            return stocks[:,i] * 0
+        if operator.mod(i, 5) == 4:
+            return stocks[:,i] * 0
+            #return np.log(stocks[:,i] + 1)
+        else:
+            return stocks[:,i] / stocks[:,0]
+    n = stocks.shape[0]
+    stocks_dat =  np.array([ process_column(i) for i in range(46)]).transpose()
+    #stocks_movingavgO9O10 = np.array([int(i > j) for i,j in zip(stocks_dat[:,45], stocks_dat[:,40])]).reshape((n, 1))
+    #stocks_movingavgC9O10 = np.array([int(i > j) for i,j in zip(stocks_dat[:,45], stocks_dat[:,43])]).reshape((n, 1))
+    #return np.hstack((stocks_dat, stocks_movingavgO9O10, stocks_movingavgC9O10))
+    return stocks_dat
 
 test = np.array(p.read_table('./test.csv', sep = ","))
 
