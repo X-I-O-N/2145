badb83d59cfc94792356ea8574b15e2ffea202c0
X-I-O-N <teerth9000@gmail.com>
2017-06-16 01:01:36 -0700

    Update allmodelspredict.py

--- allmodelspredict.py
+++ allmodelspredict.py
@@ -45,7 +45,7 @@ X_test = X_test_stockdata
 #load model
 filename = 'allmodel.sav'
 best_model = pickle.load(open(filename, 'rb'))
-best_model.fit(new_X, new_Y) = best_model.score(new_X, new_Y)
+best_model.fit(X, y) = best_model.score(X, y)
 
 
 print "prediction"
