dda2385b5a70e2d25a5d08f47fb28dea51b72e16
X-I-O-N <teerth9000@gmail.com>
2017-06-17 18:57:45 -0700

    Add files via upload

--- train/trainformat.py
+++ train/trainformat.py
@@ -5,15 +5,15 @@ allfiles = glob.glob('*.csv')
 index = 0
 
 def testing(file):
-    file.drop('Gmt time', axis=1)
+    file = file.loc[:,'Open':'Volume']
     file = file.values.reshape(1, -1)
     return file
 
 
 for _fileT in allfiles:
-    index += 1
-    nFile = pd.read_csv(_fileT)
+    nFile = pd.read_csv(_fileT, header=0)
     fFile = testing(nFile)
     df = pd.DataFrame(fFile)
-    new_df = df.iloc[500, :]
-    df.to_csv('HeadCSV/FinalCSV.csv', mode='a', index=True)
+    new_df = df.iloc[:500]
+    new_df = new_df.shift(1, axis=1)
+    new_df.to_csv('HeadCSV/FinalCSV.csv', mode='a', index=False)
