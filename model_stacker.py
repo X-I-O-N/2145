a6dff68e3b4bd8d177bfcfa87bea5a8080f2f183
X-I-O-N <teerth9000@gmail.com>
2017-06-23 05:43:10 -0700

    Update model_stacker.py

--- model_stacker.py
+++ model_stacker.py
@@ -174,8 +174,7 @@ print "this step done"
           #GradientBoostingClassifier(n_estimators = 200),
           ]
 
-models = [lm.Perceptron(n_jobs=-1)
-          ]
+models = [lm.Perceptron(n_jobs=-1)]
 
 def get_oos_predictions(models, X, y, folds = 10):
     
