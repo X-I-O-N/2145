6676390fa6a6e72250493d363fd5536af7915f49
X-I-O-N <teerth9000@gmail.com>
2017-06-24 06:01:20 -0700

    Update model_tuner.py

--- model_tuner.py
+++ model_tuner.py
@@ -119,7 +119,7 @@ def normalize10day(stocks):
 # <codecell>
 
 print "loading data.."
-train = np.array(p.read_table('./min94.csv', sep = ","))
+train = np.array(p.read_table('./1pair.csv', sep = ","))
 test = np.array(p.read_table('./test.csv', sep = ","))
 
 ################################################################################
